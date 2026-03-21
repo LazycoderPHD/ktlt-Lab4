69ddf97b3736c210faa401db0f792593435992a3
LazycoderPHD <phatdao2309@gmail.com>
2026-03-21 16:24:45 +0700

    changed person.cs and program.cs

--- ktlt-Lab4/Person.cs
+++ ktlt-Lab4/Person.cs
@@ -6,48 +6,38 @@ using System.Threading.Tasks;
 
 namespace ktlt_Lab4
 {
-    internal class Person
+    public class Person
     {
-        private string name;
-        private int birthYear;
-        private string address;
+        protected string name;
+        protected int birthYear;
+        protected string address;
 
-
-        public int BirthYear
-        {
-            get => birthYear;
-            set
-            {
-                if (value >= 1900 && value <= DateTime.Now.Year)
-                { birthYear = value; }
-                else
-                {
-                    Console.WriteLine("value invalid");
-                    birthYear = 1900;
-                }
-            }
-        }
         public Person() { }
         public Person(string name, int birthYear, string address)
         {
             this.name = name;
-            this.BirthYear = birthYear;
+            this.birthYear = birthYear;
             this.address = address;
         }
-        public int GetAge()
-        { return DateTime.Now.Year - birthYear; }
+
         public virtual void Input()
         {
-            Console.Write("nhap ten: ");
+            Console.Write("Nhap ten: ");
             name = Console.ReadLine()!;
-            Console.Write("nhap nam sinh: ");
-            BirthYear = int.Parse(Console.ReadLine()!);
+            Console.Write("Nhap nam sinh: ");
+            birthYear = Convert.ToInt32(Console.ReadLine()!);
             Console.Write("Nhap dia chi: ");
             address = Console.ReadLine()!;
         }
+
+        public int GetAge()
+        {
+            return DateTime.Now.Year - birthYear;
+        }
+
         public override string ToString()
         {
-            return $"name:{name}, Nam sinh: {birthYear}, Tuoi : {GetAge()}, Dia chi: {address}";
+            return $"Person[Name: {name}, Age: {GetAge()}, Address: {address}]";
         }
     }
 }
--- ktlt-Lab4/Program.cs
+++ ktlt-Lab4/Program.cs
@@ -1,9 +1,14 @@
 ﻿using ktlt_Lab4;
 
+RunPersonDemo();
 void RunPersonDemo()
 {
     Person person1 = new Person();
-    Person person2 = new Person();
-    Person person3 = new Person();
+    Person person2 = new Person("Phat", 2007, "125 Sieu sao");
+    //Person person3 = new Person();
+    //person1.Input();
+    person2.Input();
 
+    Console.WriteLine($"{person1.ToString()}");
+    Console.WriteLine($"{person2.ToString()}");
 }
