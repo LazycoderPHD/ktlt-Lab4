30c647272954c79dd561cfa644485f5047801de8
LazycoderPHD <phatdao2309@gmail.com>
2026-03-21 17:36:02 +0700

    FINISHED BAITAP2, moving to next branch

--- baitap2/Program.cs
+++ baitap2/Program.cs
@@ -1,4 +1,8 @@
 ﻿using baitap2;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
 
 RunFractionDemo();
 void RunFractionDemo()
@@ -7,3 +11,26 @@ void RunFractionDemo()
     Console.WriteLine($"Phân số ban đầu: {f}");
 }
 
+Console.WriteLine("\n");
+
+
+RunMixedFractionDemo();
+//void RunMixedFractionDemo()
+//{
+//    MixedFraction mf1 = new MixedFraction(2, 1, 3);
+//    Console.WriteLine($"Hỗn số từ (2, 1, 3): {mf1}");
+
+//    MixedFraction mf2 = new MixedFraction(f);
+//    Console.WriteLine($"Hỗn số chuyển từ phân số {f}: {mf2}");
+//}
+
+void RunMixedFractionDemo()
+{
+    Fraction f = new Fraction(7, 3);
+
+    MixedFraction mf1 = new MixedFraction(2, 1, 3);
+    Console.WriteLine($"Hỗn số từ (2, 1, 3): {mf1}");
+
+    MixedFraction mf2 = new MixedFraction(f);
+    Console.WriteLine($"Hỗn số chuyển từ phân số {f}: {mf2}");
+}
