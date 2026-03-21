5b2acde20689fe4119ec4a83e612e9791c44e9ae
LazycoderPHD <phatdao2309@gmail.com>
2026-03-21 16:28:53 +0700

    Added new student.cs for homework, taking some of person code most likely inheritance

---
+++ ktlt-Lab4/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ktlt_Lab4
+{
+    public class Student : Person
+    {
+        private string program;
+        private int year;
+
+        public Student() : base() { }
+        public Student(string name, int birthYear, string address, string program, int year)
+            : base(name, birthYear, address)
+        {
+            this.program = program;
+            this.year = year;
+        }
+
+        public override void Input()
+        {
+            base.Input();
+            Console.Write("Nhap chuong trinh hoc: "); program = Console.ReadLine()!;
+            Console.Write("Nhap nam hoc: "); year = int.Parse(Console.ReadLine()!);
+        }
+
+        public override string ToString()
+        {
+            return $"Student[{base.ToString()} - Program: {program}, Year: {year}]";
+        }
+
+        public void ChangeProgram(string program)
+        {
+            this.program = program;
+        }
+    }
+}
