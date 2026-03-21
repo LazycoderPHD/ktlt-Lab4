8be1b3caa57038950eb4deb5d376026236806136
LazycoderPHD <phatdao2309@gmail.com>
2026-03-21 16:32:16 +0700

    Added new staff.cs for homework

---
+++ ktlt-Lab4/Staff.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ktlt_Lab4
+{
+    public class Staff : Person
+    {
+        private string department;
+        private double salary;
+
+        public Staff() : base() { }
+        public Staff(string name, int birthYear, string address, string department, double salary)
+            : base(name, birthYear, address)
+        {
+            this.department = department;
+            this.salary = salary;
+        }
+
+        public override void Input()
+        {
+            base.Input();
+            Console.Write("Nhap phong ban: "); department = Console.ReadLine()!;
+            Console.Write("Nhap luong: "); salary = double.Parse(Console.ReadLine()!);
+        }
+
+        public override string ToString()
+        {
+            return $"Staff[{base.ToString()} - Department: {department}, Salary: {salary}]";
+        }
+
+        public void UpdateSalary(double salary)
+        {
+            this.salary = salary;
+        }
+    }
+}
